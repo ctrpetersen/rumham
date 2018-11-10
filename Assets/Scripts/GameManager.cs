c8202b72fd753d2cdea42accc4d3b00ae75f6f03
Christoffer Petersen <ctrpetersen@outlook.com>
2018-11-10 16:30:42 +0100

    Game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Manages level triggers etc.
+
+    public GameObject PlayerPrefab;
+    private GameObject Player;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Player = Instantiate(PlayerPrefab, new Vector3(-6,0,1),Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,13 +13,11 @@ public class Player : MonoBehaviour
     public float MaxAngularVelocity;
     
 
-	// Use this for initialization
 	void Start ()
 	{
 	    rb = GetComponent<Rigidbody2D>();
     }
 	
-	// Update is called once per frame
 	void Update ()
 	{
 	    if (Input.GetKey("a"))
