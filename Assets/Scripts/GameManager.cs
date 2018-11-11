2a0a66144990db26c42ab8238f0ecde5ccf61c49
Christoffer Petersen <ctrpetersen@outlook.com>
2018-11-11 17:56:33 +0100

    intro switch, video, splash screen for scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,25 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     // Manages level triggers etc.
 
     public GameObject PlayerPrefab;
-    private GameObject Player;
     public Vector3 Offset;
     public Transform Camera;
+    public Image LevelIntro;
 
-	// Use this for initialization
-	void Start ()
-	{
-	    Player = Instantiate(PlayerPrefab, new Vector3(-6,0,1),Quaternion.identity);
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		Camera.position = new Vector3(Player.transform.position.x + Offset.x, Player.transform.position.y + Offset.y, Offset.z);
-	}
+    private GameObject Player;
+
+    // Use this for initialization
+    void Start()
+    {
+        Player = Instantiate(PlayerPrefab, new Vector3(-6, 0, 1), Quaternion.identity);
+        StartCoroutine(FadeImage());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Camera.position = new Vector3(Player.transform.position.x + Offset.x, Player.transform.position.y + Offset.y,
+            Offset.z);
+    }
+
+    IEnumerator FadeImage()
+    {
+        for (float i = 3; i >= 0; i -= Time.deltaTime)
+        {
+            LevelIntro.color = new Color(1, 1, 1, i);
+            yield return null;
+        }
+    }
 }
---
+++ Assets/Scripts/IntroSwitch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class IntroSwitch : MonoBehaviour {
+
+    private double time;
+    private double currentTime;
+
+    public VideoPlayer video;
+
+    // Use this for initialization
+    void Start () {
+        time = video.clip.length-0.5;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    currentTime = video.time;
+	    if (currentTime >= time)
+	    {
+	        SceneManager.LoadScene("Level_one");
+	    }
+    }
+
+    
+}
